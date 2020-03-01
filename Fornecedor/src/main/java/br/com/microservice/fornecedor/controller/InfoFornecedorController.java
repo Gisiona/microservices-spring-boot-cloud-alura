1e333466deb59e4d75470e768fa8a30f048dd4f9
gisiona <gil-real@hotmail.com>
2020-02-29 23:05:18 -0300

    formatado o retorno da api de fornecedor

--- Fornecedor/src/main/java/br/com/microservice/fornecedor/controller/InfoFornecedorController.java
+++ Fornecedor/src/main/java/br/com/microservice/fornecedor/controller/InfoFornecedorController.java
@@ -3,11 +3,14 @@ package br.com.microservice.fornecedor.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.microservice.fornecedor.dto.response.DataResponse;
 import br.com.microservice.fornecedor.entity.InfoFornecedor;
 import br.com.microservice.fornecedor.processor.InfoFornecedorProcessor;
 
@@ -19,15 +22,15 @@ public class InfoFornecedorController {
 	private InfoFornecedorProcessor infoFornecedorProcessor;
 	
 	@GetMapping("/all/{estado}")
-	public List<InfoFornecedor> buscarFornecedorPorEstado(@PathVariable String estado) {	
+	public ResponseEntity<DataResponse<List<InfoFornecedor>>> buscarFornecedorPorEstado(@PathVariable String estado) {	
 		List<InfoFornecedor> fornecedores = infoFornecedorProcessor.buscarFornecedorPorEstado(estado);
-		return fornecedores;
+		return ResponseEntity.status(HttpStatus.OK).body(new DataResponse (fornecedores));
 	}
 	
 	@GetMapping("/info/{id}")
-	public InfoFornecedor buscarFornecedorPorId(@PathVariable Long id) {	
+	public ResponseEntity<DataResponse<InfoFornecedor>> buscarFornecedorPorId(@PathVariable Long id) {	
 		InfoFornecedor fornecedor = infoFornecedorProcessor.buscarFornecedorPorId(id);
-		return fornecedor;
+		return ResponseEntity.status(HttpStatus.OK).body(new DataResponse (fornecedor));
 	}
 	
 	
